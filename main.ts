51de20315544d508f93e68143770ae5200bfe3de
Lennard Zündorf <l.p.zuendorf@gmail.com>
2024-10-19 12:50:11 +0200

    chore: cleanup and setting up linting, formatting, pre-commit hook

--- main.ts
+++ main.ts
@@ -1,37 +1,38 @@
-// src/main.ts
-import { Plugin, WorkspaceLeaf } from 'obsidian';
-import { TaskUIView, VIEW_TYPE_TASKUI } from 'src/views/TaskUIView';
+// Description: Main Plugin File for the Obsidian Plugin
+import { Plugin, WorkspaceLeaf } from "obsidian";
+import { MainView, VIEW_TYPE_MAIN } from "src/MainView";
 
 export default class ShardsTaskUIPlugin extends Plugin {
 	async onload() {
-
 		// Register the Main Tab View
-		this.registerView(VIEW_TYPE_TASKUI, (leaf) => new TaskUIView(leaf));
+		this.registerView(VIEW_TYPE_MAIN, (leaf) => new MainView(leaf));
 
 		// Add Ribbon Icons to Activate the Views
-		this.addRibbonIcon('layout', 'Activate Main Tab View', () => {
+		this.addRibbonIcon("layout", "Activate Main Tab View", () => {
 			this.activateMainTabView();
 		});
 	}
 
 	async onunload() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_TASKUI);
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_MAIN);
 	}
 
 	// Method to activate the Main Tab View
 	async activateMainTabView() {
-		const {workspace} = this.app;
+		const { workspace } = this.app;
 
 		let leaf: WorkspaceLeaf | null = null;
-		const leaves = workspace.getLeavesOfType(VIEW_TYPE_TASKUI);
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_MAIN);
 
 		if (leaves.length > 0) {
 			leaf = leaves[0];
+			await workspace.revealLeaf(leaf);
+			console.log("Main Tab View Activated");
 		} else {
 			leaf = workspace.getLeaf(false);
 
 			if (leaf) {
-				await leaf.setViewState({type: VIEW_TYPE_TASKUI, active: true});
+				await leaf.setViewState({ type: VIEW_TYPE_MAIN, active: true });
 			}
 		}
 	}
--- src/components/TaskUIApp.tsx
+++ src/components/TaskUIApp.tsx
@@ -1,13 +1,13 @@
 // Description: This file contains the SidebarApp component.
-import React from 'react';
+import React from "react";
 
 const TaskUIApp: React.FC = () => {
-    return (
-        <div>
-            <h1>Shards TaskUI</h1>
-            This is the Main View
-        </div>
-    );
+	return (
+		<div>
+			<h1>Shards TaskUI</h1>
+			This is the Main View
+		</div>
+	);
 };
 
 export default TaskUIApp;
