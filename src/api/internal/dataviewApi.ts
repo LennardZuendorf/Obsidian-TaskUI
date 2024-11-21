4e80457fc661aa48b5a9ad2f698d96162b0bdf61
Lennard Zündorf <l.p.zuendorf@gmail.com>
2024-11-21 20:36:09 +0100

    feat: remodeled dataviewAPI slightly because of errors, included pre-check for plugin

--- src/api/internal/dataviewApi.ts
+++ src/api/internal/dataviewApi.ts
@@ -1,5 +1,5 @@
-import { getAPI, STask, DataviewApi } from "obsidian-dataview";
-import { logger as logger } from "../../utils/logger";
+import { getAPI, STask, DataviewApi, SMarkdownPage } from "obsidian-dataview";
+import { logger } from "../../utils/logger";
 
 /**
  * Represents the structure of a task as provided by the Dataview API.
@@ -10,64 +10,68 @@ export type { STask as dvTaskType };
  * DataviewApiProvider class provides methods for querying tasks from the Obsidian vault using the Dataview API.
  */
 export class DataviewApiProvider {
-	private readonly dvApi: DataviewApi;
+    private readonly dvApi: DataviewApi;
 
-	/**
-	 * Constructs an instance of the Dataview API Provider.
-	 * Initializes the Dataview API for interacting with Obsidian's data.
-	 *
-	 * @throws Will throw an error if the Dataview API is not available.
-	 */
-	constructor() {
-		try {
-			const api = getAPI();
-			if (!api) {
-				throw new Error("Dataview API returned undefined.");
-			}
-			this.dvApi = api;
-		} catch (error) {
-			logger.error(error.message);
-			throw new Error(error);
-		}
-	}
+    /**
+     * Constructs an instance of the Dataview API Provider.
+     * Initializes the Dataview API for interacting with Obsidian's data.
+     *
+     * @throws Will throw an error if the Dataview API is not available.
+     */
+    constructor() {
+        const api = getAPI();
+        if (!api) {
+            const errorMessage = "Dataview API returned undefined.";
+            logger.error(errorMessage);
+            throw new Error(errorMessage);
+        }
+        this.dvApi = api;
+    }
 
-	/**
-	 * Retrieves tasks from a specific file path.
-	 *
-	 * @param filePath - The exact file path from which to retrieve tasks.
-	 * @returns A promise that resolves to a tasksTransferObject containing the tasks or an error message.
-	 */
-	public async getTasksFromFile(filePath: string): Promise<STask[] | null> {
-		try {
-			return await this.dvApi.page(filePath).tasks();
-		} catch (error) {
-			logger.error(
-				`Error fetching tasks from file: ${filePath} - ${error.message}`,
-			);
-			return null;
-		}
-	}
+    /**
+     * Retrieves tasks from a specific file path.
+     *
+     * @param filePath - The exact file path from which to retrieve tasks.
+     * @returns A promise that resolves to an array of tasks or null if an error occurs.
+     */
+    public async getTasksFromFile(filePath: string): Promise<STask[] | null> {
+        try {
+            const page = await this.dvApi.page(filePath);
+            return page?.tasks() || [];
+        } catch (error) {
+            logger.error(`Error fetching tasks from file: ${filePath} - ${error.message}`);
+            return null;
+        }
+    }
 
-	/**
-	 * Retrieves all tasks across the entire vault.
-	 *
-	 * @returns A promise that resolves to a tasksTransferObject containing all tasks or an error message.
-	 */
-	public async getAllTasks(): Promise<STask[] | null> {
-		try {
-			return this.dvApi.pages().tasks();
-		} catch (error) {
-			logger.error(`Error fetching all tasks: ${error.message}`);
-			return null;
-		}
-	}
+    /**
+     * Retrieves all tasks across the entire vault.
+     *
+     * @returns A promise that resolves to an array of all tasks or null if an error occurs.
+     */
+    public async getAllTasks(): Promise<STask[] | null> {
+        const tasks: STask[] = [];
 
-	/**
-	 * Checks if the Dataview API is available.
-	 *
-	 * @returns A boolean indicating whether the Dataview API is available.
-	 */
-	public isDataviewApiAvailable(): boolean {
-		return !!this.dvApi;
-	}
+        try {
+            this.dvApi.pages().forEach((page: SMarkdownPage) => {
+                if (page.file?.tasks) {
+                    tasks.push(...page.file.tasks);
+                }
+            });
+        } catch (error) {
+            logger.error(`Error fetching all tasks: ${error.message}`);
+            return null;
+        }
+
+        return tasks;
+    }
+
+    /**
+     * Checks if the Dataview API is available.
+     *
+     * @returns A boolean indicating whether the Dataview API is available.
+     */
+    public isDataviewApiAvailable(): boolean {
+        return !!this.dvApi;
+    }
 }
